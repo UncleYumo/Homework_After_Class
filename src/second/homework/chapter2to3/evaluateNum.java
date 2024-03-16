01586b10c40de1dd4138179eda1b7aab35ed6d0c
uncle_yumo <wy13921971147@163.com>
2024-03-17 00:20:01 +0800

    Homework 2-3

---
+++ src/second/homework/chapter2to3/evaluateNum.java
@@ -0,0 +1,12 @@
+package second.homework.chapter2to3;
+
+public class evaluateNum {
+    public static void main(String[] args) {
+                int a=5;
+                int b=5;
+                int x=2*++a;
+                int y=2*b++;
+                System.out.println("x="+x) ;
+                System.out.println("y="+y) ;
+    }
+}
---
+++ src/third/homework/chapter4/StringTest.java
@@ -0,0 +1,27 @@
+package third.homework.chapter4;
+
+public class StringTest {
+    public static void main(String[] args) {
+
+        StringTest stringCat = new StringTest();
+        String getSTr = stringCat.concat("-","Fuck","You","All","MotherFucker");
+        System.out.println(getSTr);
+
+    }
+
+    public String concat(String operator,String ... strs) {
+
+        String result = "";
+
+        for (int i = 0;i < strs.length;i++) {
+            if (i == 0) {
+               result += strs[i];
+            }else {
+                result += (operator + strs[i]);
+            }
+        }
+
+        return result;
+
+    }
+}
