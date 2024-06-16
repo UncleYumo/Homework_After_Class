0d9edca04943acfdfad62f017eb50d18df5cbb94
uncle_legion <wy13921971147@163.com>
2024-06-16 22:29:10 +0800

    学习了异常处理，并完善了彩色打印类

--- src/com/review/programming/progrom/ExceptionDemo1.java
+++ src/com/review/programming/progrom/ExceptionDemo1.java
@@ -1,9 +1,5 @@
 package com.review.programming.progrom;
 
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
-import java.util.Date;
-
 /**
  * @author uncle_yumo
  * @CreateDate 2024/6/14
@@ -13,10 +9,41 @@ import java.util.Date;
  */
 public class ExceptionDemo1 {
 
-    public static void main(String[] args) throws ParseException {
-        String time = "2024年6月14日";
-        SimpleDateFormat sdf = new SimpleDateFormat("yyyy年MM月dd日");
-        Date date = sdf.parse(time);
-        System.out.println("默认的日期为：" + time);
+    public static void main(String[] args) {
+        Color_Print_Utils.getInstance().printlnYellow("-------- start --------\n");
+        try {
+            int[] arr = null;
+            int[] arr1 = {0};
+            int[] arr2 = {0,1,19,21,4};
+            testException.getMax(arr);
+            testException.getMax(arr1);
+            testException.getMax(arr2);
+        } catch (Exception e) {
+            Color_Print_Utils.getInstance().printlnFontAndBackgroundWithColor(
+                    Color_Print_Utils.getInstance().Font_Color_Black,
+                    Color_Print_Utils.getInstance().Background_Color_Cyan,
+                    e.fillInStackTrace().toString()
+            );
+        }
+
+        Color_Print_Utils.getInstance().printlnYellow("\n-------- end --------");
+    }
+}
+
+class testException {
+    public static int getMax(int[] arr) {
+//        if(arr == null) {
+//            throw new NullPointerException("产生空指针异常，请检查传入的数组是否为null");
+//        }
+        if(arr.length == 0) {
+            throw new ArrayIndexOutOfBoundsException("产生数组越界异常，无法获取最大值");
+        }
+        int maxTemp = arr[0];
+        for (int i : arr) {
+            if (i > maxTemp) {
+                maxTemp = i;
+            }
+        }
+        return maxTemp;
     }
 }
